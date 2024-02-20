73495d872d56c58f6e09e1f2c4769be831ca3d58
FuryBaka <110598822+FuryBaka@users.noreply.github.com>
2024-02-20 14:28:49 +0100

    Add files via upload

---
+++ fk_weaponanimation/locales/fr.lua
@@ -0,0 +1,6 @@
+Locales['fr'] = {
+    ['mainMenu_title'] = 'Animation d\'armes',
+    ['mainMenu_subtitle'] = 'Intéraction',
+    ['mainMenu_separator'] = '~r~↓~s~ Menu Holster ~r~↓~s~',
+    ['mainMenu_separator2'] = '~r~↓~s~ Menu Aim ~r~↓~s~',
+}
